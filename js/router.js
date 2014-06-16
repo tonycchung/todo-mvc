dd4cd5feda80556989d34c3160cf0e8cfc6961b6
Tony Chung <anthonyuchung@gmail.com>
2014-06-15 21:05:06 -0700

    Transitioning to show only incomplete todos

--- js/router.js
+++ js/router.js
@@ -1,6 +1,7 @@
 Todos.Router.map(function() {
   this.resource('todos', { path: '/' }, function () {
     // additional child routes
+    this.route('active');
   });
 });
 
@@ -13,5 +14,15 @@ Todos.TodosRoute = Ember.Route.extend({
 Todos.TodosIndexRoute = Ember.Route.extend({
   model: function() {
     return this.modelFor('todos');
+  }
+});
+
+Todos.TodosActiveRoute = Ember.Route.extend({
+  model: function() {
+    return this.store.filter('todo', function(todo) {
+    });
+  },
+  renderTemplate: function(controller) {
+    this.render('todos/index', {controller: controller});
   }
 });
