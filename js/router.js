60de60aa4b68ff038fe6c7613dfbddbd424ccd2b
Tony Chung <anthonyuchung@gmail.com>
2014-06-15 21:07:02 -0700

    Transitioning to show only complete todos

--- js/router.js
+++ js/router.js
@@ -2,6 +2,7 @@ Todos.Router.map(function() {
   this.resource('todos', { path: '/' }, function () {
     // additional child routes
     this.route('active');
+    this.route('completed');
   });
 });
 
@@ -20,6 +21,7 @@ Todos.TodosIndexRoute = Ember.Route.extend({
 Todos.TodosActiveRoute = Ember.Route.extend({
   model: function() {
     return this.store.filter('todo', function(todo) {
+      return todo.get('isCompleted');
     });
   },
   renderTemplate: function(controller) {
